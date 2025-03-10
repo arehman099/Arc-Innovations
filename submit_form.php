a5d3d27bf590a3468ac081c38adb8db241b6e0a7
Arc Innovations <148364532+arehman099@users.noreply.github.com>
2025-03-10 21:21:39 +0100

    Add files via upload

---
+++ submit_form.php
@@ -0,0 +1,35 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Set your email address
+    $to = "info@arcinnovations.it"; // Replace with your email address
+
+    // Prepare email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Prepare email body
+    $email_body = "You have received a new message from the contact form on your website.\n\n";
+    $email_body .= "Name: $name\n";
+    $email_body .= "Email: $email\n";
+    $email_body .= "Subject: $subject\n";
+    $email_body .= "Message:\n$message\n";
+
+    // Send email
+    mail($to, $subject, $email_body, $headers);
+
+    // Redirect back to the form page
+    header("Location: index.html");
+    exit;
+} else {
+    // If the form is not submitted, redirect to the form page
+    header("Location: index.html");
+    exit;
+}
+?>
